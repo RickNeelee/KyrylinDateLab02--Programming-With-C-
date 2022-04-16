5c4f968f9bc8c272a8ac13248ba58f026861e613
Єгор Кирилін <yehor.kyrylin@ukma.edu.ua>
2022-04-16 21:33:17 +0300

    Добавьте файлы проекта.

---
+++ KyrylinDateLab02/DatePickerVM.cs
@@ -0,0 +1,231 @@
+﻿using JetBrains.Annotations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KyrylinDateLab02
+{
+    internal class DatePickerVM : INotifyPropertyChanged
+    {
+        #region Fields
+        private Person _person;
+        private string _name;
+        private string _surname;
+        private string _email;
+        private DateTime _birthdate = DateTime.Today;
+        private string _namesurnameemail = "";
+        private string _age = "";
+        private string _westernZodiac = "";
+        private string _chineseZodiac = "";
+        private RelayCommand<object> _submit;
+        #endregion
+
+        #region Properties
+        public RelayCommand<object> Submitting
+        {
+            get
+            {
+                return _submit ?? (_submit = new RelayCommand<object>(
+                           Submit_Click, o => IsAbleToSubmit()));
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+            set
+            {
+                _surname = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            set
+            {
+                _email = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public DateTime Birthdate
+        {
+            get
+            {
+                return _birthdate;
+            }
+            set
+            {
+                _birthdate = value;
+                OnPropertyChanged();
+            }
+        }
+        public string NameSurnameEmail
+        {
+            get
+            {
+                return _namesurnameemail;
+
+            }
+            private set
+            {
+                _namesurnameemail = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string WesternZodiac
+        {
+            get
+            {
+                return _westernZodiac;
+            }
+            set
+            {
+                _westernZodiac = value;
+                OnPropertyChanged();
+            }
+        }
+        public string ChineseZodiac
+        {
+            get
+            {
+                return _chineseZodiac;
+            }
+            set
+            {
+                _chineseZodiac = value;
+                OnPropertyChanged();
+            }
+        }
+        public string Age
+        {
+            get
+            {
+                return _age;
+
+            }
+            set
+            {
+                _age = value;
+                OnPropertyChanged();
+            }
+        }
+        public Person Person
+        {
+            get
+            {
+                return _person;
+            }
+            set
+            {
+                _person = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        internal DatePickerVM()
+        {
+            Birthdate = DateTime.Today;
+        }
+
+        private async void Submit_Click(object obj)
+        {
+            NameSurnameEmail = "";
+            Age = "";
+            WesternZodiac = "";
+            ChineseZodiac = "";
+            await Task.Run(() =>
+            {
+                Thread.Sleep(1000);
+                try
+                {
+                    Person = new Person(Name, Surname, Email, Birthdate);
+                    if ((_person.AgeFull < 0) || (_person.AgeFull > 135))
+                    {
+                        MessageBox.Show("Error! Wrong B-Day!");
+                    }
+                    else
+                    {
+                        if (_person.IsBirthday)
+                        {
+                            Age = "Happy birthday! You're turning " + _person.AgeFull + " today!";
+                        }
+                        else
+                        {
+                            Age = "Your age is " + _person.AgeFull + " full years.";
+                        }
+                        if (_person.IsAdult)
+                        {
+                            NameSurnameEmail = _person.Name + " " + _person.Surname + " , " + (_person.Birthdate).Day + "." + (_person.Birthdate).Month  + "." + (_person.Birthdate).Year + " ; " + _person.Email + " ; Adult.";
+                        }
+                        else
+                        {
+                            NameSurnameEmail = _person.Name + " " + _person.Surname + " , " + (_person.Birthdate).Day + "." + (_person.Birthdate).Month + "." + (_person.Birthdate).Year + " ; " + _person.Email + " ; Underaged.";
+                        }
+                        WesternZodiac = "Your zodiac sign is " + Person.WesternZodiac;
+                        ChineseZodiac = "Your Chinese zodiac sign is " + Person.ChineseZodiac;
+                    }
+                }
+                catch (Exception er)
+                {
+                    MessageBox.Show(er.Message);
+                }
+            });
+        }
+        private bool IsAbleToSubmit()
+        {
+            if (string.IsNullOrWhiteSpace(_name) || string.IsNullOrWhiteSpace(_surname) || string.IsNullOrWhiteSpace(_email))
+            {
+                return false;
+            }
+            else { return true; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
+
---
+++ KyrylinDateLab02/Person.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KyrylinDateLab02
+{
+    internal class Person
+    {
+        #region Fields
+        private string _name;
+        private string _surname;
+        private string _email;
+        private int _age;
+        private DateTime _birthdate;
+
+        private readonly bool _isAdult;
+        private readonly bool _isBirthday;
+        private readonly string _westernZodiac;
+        private readonly string _chineseZodiac;
+
+        #endregion
+
+        #region Properties
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            private set
+            {
+                _name = value;
+            }
+        }
+
+        public string Surname
+        {
+            get
+            {
+                return _surname;
+            }
+            private set
+            {
+                _surname = value;
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return _email;
+            }
+            private set
+            {
+                _email = value;
+            }
+        }
+
+        public DateTime Birthdate
+        {
+            get
+            {
+                return _birthdate;
+            }
+            private set
+            {
+                _birthdate = value;
+            }
+        }
+
+        public int AgeFull
+        {
+            get
+            {
+                return _age;
+            }
+            private set
+            {
+                _age = value;
+            }
+        }
+        public string WesternZodiac
+        {
+            get
+            {
+                return _westernZodiac;
+            }
+        }
+
+        public string ChineseZodiac
+        {
+            get
+            {
+                return _chineseZodiac;
+            }
+        }
+
+        public bool IsBirthday
+        {
+            get
+            {
+                return _isBirthday;
+            }
+        }
+
+        public bool IsAdult
+        {
+            get
+            {
+                return _isAdult;
+            }
+        }
+
+        #endregion
+
+        internal Person(string Name, string Surname, string Email, DateTime Birthdate)
+        {
+            _name = Name;
+            _surname = Surname;
+            _email = Email;
+            _birthdate = Birthdate;
+            _age = GetAgeInt(_birthdate, DateTime.Today);
+
+            int age = GetAgeInt(_birthdate, DateTime.Today);
+            _isAdult = (age >= 18);
+            _isBirthday = IsBirthdayCheck(_birthdate, DateTime.Today);
+            _westernZodiac = GettingZodiac(_birthdate);
+            _chineseZodiac = GettingChZodiac(_birthdate);
+        }
+
+        public Person(string Name, string Surname, DateTime Birthdate) :
+           this(Name, Surname, "rickneelee@coolhouse.com", Birthdate)
+        {
+
+        }
+        public Person(string Name, string Surname, string Email) :
+            this(Name, Surname, Email, DateTime.Today)
+        {
+
+        }
+ 
+        private int GetAgeInt(DateTime startDate, DateTime endDate)
+        {
+            int AgeFull = (endDate.Year - startDate.Year - 1) +
+                (((endDate.Month > startDate.Month) ||
+                ((endDate.Month == startDate.Month) && (endDate.Day >= startDate.Day))) ? 1 : 0);
+            return AgeFull;
+        }
+
+        private bool IsBirthdayCheck(DateTime startDate, DateTime endDate)
+        {
+            if ((startDate.Day == DateTime.Today.Day) && (startDate.Month == DateTime.Today.Month))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private string GettingZodiac(DateTime birthDate)
+        {
+            return GetZodiac(birthDate);
+        }
+
+        private string GettingChZodiac(DateTime birthDate)
+        {
+            return GetChineseZodiac(birthDate);
+        }
+        public string GetZodiac(DateTime birthDate)
+        {
+            switch (birthDate.Month)
+            {
+                case 1:
+                    {
+                        if (birthDate.Day < 21) return "Capricorn";
+                        else return "Aquarius";
+                    }
+                case 2:
+                    {
+                        if (birthDate.Day < 20) return "Aquarius";
+                        else return "Pisces";
+                    }
+                case 3:
+                    {
+                        if (birthDate.Day < 21) return "Pisces";
+                        else return "Aries";
+                    }
+                case 4:
+                    {
+                        if (birthDate.Day < 21) return "Aries";
+                        else return "Taurus";
+                    }
+                case 5:
+                    {
+                        if (birthDate.Day < 22) return "Taurus";
+                        else return "Gemini";
+                    }
+                case 6:
+                    {
+                        if (birthDate.Day < 22) return "Gemini";
+                        else return "Cancer";
+                    }
+                case 7:
+                    {
+                        if (birthDate.Day < 23) return "Cancer";
+                        else return "Leo";
+                    }
+                case 8:
+                    {
+                        if (birthDate.Day < 22) return "Leo";
+                        else return "Virgo";
+                    }
+                case 9:
+                    {
+                        if (birthDate.Day < 24) return "Virgo";
+                        else return "Libra";
+                    }
+                case 10:
+                    {
+                        if (birthDate.Day < 24) return "Libra";
+                        else return "Scorpio";
+                    }
+                case 11:
+                    {
+                        if (birthDate.Day < 24) return "Scorpio";
+                        else return "Saggitarius";
+                    }
+                case 12:
+                    {
+                        if (birthDate.Day < 23) return "Saggitarius";
+                        else return "Capricorn";
+                    }
+                default: return "Ophiuchus";
+            }
+        }
+
+        public string GetChineseZodiac(DateTime birthDate)
+        {
+            switch ((birthDate.Year - 4) % 12)
+            {
+                case 0:
+                    {
+                        return "Rat";
+                    }
+                case 1:
+                    {
+                        return "Ox";
+                    }
+                case 2:
+                    {
+                        return "Tiger";
+                    }
+                case 3:
+                    {
+                        return "Rabbit";
+                    }
+                case 4:
+                    {
+                        return "Dragon";
+                    }
+                case 5:
+                    {
+                        return "Snake";
+                    }
+                case 6:
+                    {
+                        return "Horse";
+                    }
+                case 7:
+                    {
+                        return "Goat";
+                    }
+                case 8:
+                    {
+                        return "Monkey";
+                    }
+                case 9:
+                    {
+                        return "Rooster";
+                    }
+                case 10:
+                    {
+                        return "Dog";
+                    }
+                case 11:
+                    {
+                        return "Pig";
+                    }
+                default: return "Unknown";
+            }
+        }
+    }
+}
+
+    
